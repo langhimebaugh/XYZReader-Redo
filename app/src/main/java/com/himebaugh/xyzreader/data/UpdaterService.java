5295041cbae97f995d7a8ecb41e763b380f81ec2
Langdon Himebaugh <lang@himebaugh.com>
2018-08-17 14:57:25 -0400

    Change imports and dependencies to fix errors.

--- app/src/main/java/com/himebaugh/xyzreader/data/UpdaterService.java
+++ app/src/main/java/com/himebaugh/xyzreader/data/UpdaterService.java
@@ -12,7 +12,7 @@ import android.os.RemoteException;
 import android.text.format.Time;
 import android.util.Log;
 
-import com.example.xyzreader.remote.RemoteEndpointUtil;
+import com.himebaugh.xyzreader.remote.RemoteEndpointUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
